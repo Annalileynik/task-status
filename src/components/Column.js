6380db378cc11e4040a85629d3e1d14307e28004
annadavydova <lileynik@yahoo.com>
2023-03-01 23:30:43 -0500

    initial

---
+++ src/components/Column.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Task from "./Task";
+
+const Column = (props) => {
+
+    return (
+
+        <div className="col">
+            <h3> {props.status.title.toUpperCase()} </h3>
+            {props.tasks.filter(task=>
+                task.status===props.status.title).map(task=>
+            <Task
+                task={task}
+            key={task._id}
+                changePriority={props.changePriority}
+                priorities={props.priorities}
+                statuses={props.statuses}
+                editTask={props.editTask}
+            />
+                )}
+
+        </div>
+
+    );
+};
+
+export default Column;
